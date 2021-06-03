bb0383e401a3b3004ce43dda3453584e79cf3f39
Fernando Corrêa da Silva <fernando_silvasc@hotmail.com>
2021-06-02 21:42:24 -0700

    started creating documentation for RentalX application.

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 
 import { routes } from './routes';
+import swaggerFileConfig from './swagger.json';
 
 const app = express();
 
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFileConfig));
 app.use(routes);
 
 const PORT = process.env.PORT || 3333;
